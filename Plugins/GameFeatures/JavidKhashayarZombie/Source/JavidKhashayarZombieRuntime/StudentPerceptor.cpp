c4b8236c4ceccc718617c4fa29e4f05a00d3ecea
xrxs <147637445+xrxsEver@users.noreply.github.com>
2026-05-11 10:55:38 +0200

    Project Setup
    + plugin setup for the exam

---
+++ Plugins/GameFeatures/JavidKhashayarZombie/Source/JavidKhashayarZombieRuntime/StudentPerceptor.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StudentPerceptor.h"
+
+
+UStudentPerceptor::UStudentPerceptor()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UStudentPerceptor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+	{
+		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptor::OnPerceptionUpdated);
+	}
+}
+
+void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
+	FString::Printf(TEXT("Saw Something!")));
+}
---
+++ Plugins/GameFeatures/JavidKhashayarZombie/Source/JavidKhashayarZombieRuntime/StudentPerceptor.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Damage.h"
+#include "Perception/AISense_Damage.h"
+#include "StudentPerceptor.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class JAVIDKHASHAYARZOMBIERUNTIME_API UStudentPerceptor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UStudentPerceptor();
+	
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+};
